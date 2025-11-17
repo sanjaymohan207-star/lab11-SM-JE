d656834c8c329f2238fc317211b92efcb923ee4e
Sanjay Mohan <sanjaymohan@Sanjays-Mac-mini.local>
2025-11-16 23:24:08 -0500

    p1 done

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(-2, 5), -10)
+        self.assertEqual(multiply(0, 7), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5
+        self.assertEqual(divide(-9, 3), -3)
+        with self.assertRaises(ZeroDivisionError):
+            divide(5, 0)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,21 +38,22 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertEqual(hypotenuse(-3, -4), 5)
+
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-9)
+
+        self.assertEqual(square_root(0), 0)
+        self.assertEqual(square_root(25), 5)
     ##########################
 
 # Do not touch this
