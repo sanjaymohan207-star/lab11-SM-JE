f96083548ec17073ed8c155821e0eb34b9e23175
Sanjay Mohan <sanjaymohan@Sanjays-Mac-mini.local>
2025-11-16 23:15:14 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -8,6 +8,13 @@ One function per operation, in order.
 
 
 import math
+def square_root(a):
+    if a < 0:
+        raise ValueError("input must be positive")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 def add(a, b):
     return a + b
 
