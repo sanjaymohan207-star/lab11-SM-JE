dd8cd76d7d14b38e8e356756dff3e03b33a4d2a0
Sanjay Mohan <sanjaymohan@Sanjays-Mac-mini.local>
2025-11-16 20:44:15 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,30 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+# First example+
 
 
+import math
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("division by zero")
+    return a / b
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("invalid base for logarithm")
+    if b <= 0:
+        raise ValueError("invalid argument for logarithm")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
